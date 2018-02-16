9b6c434e4cfe51a8471fe997967dc7c03f393e29
Krishna Ijjada <KRISHNA.IJJADA@jispl.com>
2018-02-16 17:47:54 +0530

    Commited the hellowworld file.

---
+++ helloworld.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.16/angular.min.js"></script>
+</head>
+<body ng-app="myApp">
+{{color}}
+
+<div ng-controller="mycontroller">
+{{color}}
+</div>
+
+<script>
+var myapp = angular.module("myApp",[]);
+
+MyController1
+</script>
+
+</body>
+</html>
+
+
