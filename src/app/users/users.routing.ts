24791cc4318322fc0360db183fee083f1481b2cf
krishnaijjada <krishnaijjada@outlook.com>
2018-02-16 18:53:55 +0530

    Commited the base code

--- src/app/users/users.routing.ts
+++
@@ -1,9 +0,0 @@
-import { Routes, RouterModule } from '@angular/router';
-
-import { UsersComponent } from './users.component';
-
-const usersRoutes: Routes = [
-  { path: 'users', component: UsersComponent, pathMatch: 'full' }
-];
-
-export const usersRouting = RouterModule.forChild(usersRoutes);
